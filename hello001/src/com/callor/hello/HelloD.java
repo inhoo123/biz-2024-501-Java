f79250e7b0dbdc3c3832fdb1527bc8d0d5e3edd9
inhoo <a52169434@gmail.com>
2024-07-03 16:24:59 +0900

    복습하기 git commit -m 복습하기 git commit -m 복습하기

--- hello001/src/com/callor/hello/HelloD.java
+++ hello001/src/com/callor/hello/HelloD.java
@@ -2,7 +2,10 @@ package com.callor.hello;
 
 public class HelloD {
 	public static void main(String[] args) {
-		
+		/*
+		 * (정수형) 변수를 선언하는 키워드가 2가지 인 이유
+		 * (기억)저장공간을 효율적으로 사용하기 위하여 
+		 */
 		int num1 = 100;
 		long num2 = 100;
 		
